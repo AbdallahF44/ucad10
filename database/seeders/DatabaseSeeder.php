84b42292dc87781440649ead2a1344a458035902
Abdallah Fawzi <119876102+AbdallahF44@users.noreply.github.com>
2023-08-05 17:16:14 +0300

    delete tables

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,23 +20,5 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-
-        for ($i = 1; $i < 20; $i++) {
-            Article::create([
-                'title' => fake()->text(50),
-                'content' => fake()->text(300),
-                'type' => [1, 2, 3][array_rand([1, 2, 3])]
-            ]);
-        }
-
-        Contact::create([
-            'facebook' => fake()->url(),
-            'instagram' => fake()->url(),
-            'linkedin' => fake()->url(),
-            'twitter' => fake()->url(),
-            'youtube' => fake()->url(),
-            'email' => fake()->email(),
-            'phone' => fake()->phoneNumber(),
-        ]);
     }
 }
--- resources/views/site/inner.blade.php
+++ resources/views/site/inner.blade.php
@@ -5,6 +5,13 @@
 @endsection
 
 @section('content')
-fgdgf
+
+    <div class="container">
+        <h1>Department</h1>
+    </div>
+{{--    @include('site.layouts.testimonial')--}}
+    <!-- Start Brand Area  -->
+    @include('site.layouts.brand')
+    <!-- End Brand Area  -->
 @endsection
 
