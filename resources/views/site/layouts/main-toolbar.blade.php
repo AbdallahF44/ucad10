425c92f71322f043bc29e691961bc24bdaa5e752
Abdallah Fawzi <119876102+AbdallahF44@users.noreply.github.com>
2023-08-09 14:27:32 +0300

    setting CRUD

--- resources/views/site/layouts/main-toolbar.blade.php
+++ resources/views/site/layouts/main-toolbar.blade.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 {{--<div class="toolbar" id="kt_toolbar">--}}
 {{--    <!--begin::Container-->--}}
 {{--    <div id="kt_toolbar_container" class="container-fluid d-flex flex-stack">--}}
@@ -15,7 +15,7 @@
 {{--        </div>--}}
 {{--    </div>--}}
 {{--</div>--}}
-=======
+
 <nav style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='currentColor'/%3E%3C/svg%3E&#34;);"
     aria-label="breadcrumb">
     <div class="container">
@@ -24,4 +24,4 @@
         </ol>
     </div>
 </nav>
->>>>>>> 0c99a4f21273c3150905c2a24b41d6996a1b6e2f
+
