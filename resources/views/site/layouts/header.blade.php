a2d06be43c4f14b5866522adc14b989dee50647b
Abdallah Fawzi <119876102+AbdallahF44@users.noreply.github.com>
2023-08-09 14:25:47 +0300

    setting CRUD

--- resources/views/site/layouts/header.blade.php
+++ resources/views/site/layouts/header.blade.php
@@ -4,33 +4,13 @@
             <div class="header-top">
                 <div class="header-top-left">
                     <ul class="header-info">
-<<<<<<< HEAD
-                        <li><a href="tel:+011235641231"><i class="icon-phone"></i>Call: 123 4561 5523</a></li>
-                        <li><a href="mailto:info@edublink.com" target="_blank"><i class="icon-envelope"></i>Email:
-                                info@edublink.com</a></li>
-=======
                         <li><a href="tel:+011235641231"><i class="icon-phone"></i>{{__('site.Call')}}: {{env('TELEPHONE_NUMBER')}}</a></li>
-                        <li><a href="mailto:info@edublink.com" target="_blank"><i class="icon-envelope"></i>{{__('site.Email')}}:
-<<<<<<< HEAD
-                               {{ env('EMAIL_ADDRESS')}}</a></li>
->>>>>>> 9b672df0f8f9fafb8a8b0dbd6108fe7853680ff9
-=======
+                        <li><a href="mailto:info@edublink.com" target="blank"><i class="icon-envelope"></i>{{__('site.Email')}}:
                                 {{ env('EMAIL_ADDRESS')}}</a></li>
->>>>>>> 0c99a4f21273c3150905c2a24b41d6996a1b6e2f
                     </ul>
                 </div>
 
                 <div class="header-top-right">
-<<<<<<< HEAD
-                    <ul class="header-info">
-                        <li><a href="#">Login</a></li>
-                        <li><a href="#">Register</a></li>
-<<<<<<< HEAD
-=======
-                        <li class="header-btn"><a href="#" class="edu-btn btn-secondary btn-medium">Apply Now <i class="icon-4"></i></a></li>
->>>>>>> 9b672df0f8f9fafb8a8b0dbd6108fe7853680ff9
-                    </ul>
-=======
                     <nav class="mainmenu-nav">
                         <ul class="mainmenu"  style="justify-content: end;">
 
@@ -53,20 +33,13 @@
                             </div>
                         </ul>
                     </nav>
->>>>>>> 0c99a4f21273c3150905c2a24b41d6996a1b6e2f
                 </div>
             </div>
         </div>
     </div>
-<<<<<<< HEAD
-=======
-
-<<<<<<< HEAD
->>>>>>> 9b672df0f8f9fafb8a8b0dbd6108fe7853680ff9
-=======
 
 
->>>>>>> 0c99a4f21273c3150905c2a24b41d6996a1b6e2f
+
     <div id="edu-sticky-placeholder"></div>
     <div class="header-mainmenu">
         <div class="container">
@@ -107,18 +80,6 @@
                             </li>
 
 
-<<<<<<< HEAD
-                                <li class="has-droupdown"><a href="#">{{__("site.Admission and Registration")}}</a>
-                                    <ul class="submenu">
-                                        <!-- <li><a href="">About Admission And Registration</a> -->
-                                        <li><a href="course-two.html">{{__("site.About Admission")}}</a></li>
-                                        <li><a href="course-three.html">{{__("site.Grading System")}}</a></li>
-                                        <li><a href="course-four.html">{{__("site.New Students")}}</a></li>
-                                        <li><a href="course-five.html">{{__("site.Frequently Asked Questions")}}</a></li>
-                                        <li><a href="course-details.html">{{__("site.Students’ Guidelines")}}</a></li>
-                                    </ul>
-                                </li>
-=======
                             <li class="has-droupdown"><a href="#">{{__("site.Admission and Registration")}}</a>
                                 <ul class="submenu">
                                     <!-- <li><a href="">About Admission And Registration</a> -->
@@ -129,7 +90,6 @@
                                     <li><a href="course-details.html">{{__("site.Students’ Guidelines")}}</a></li>
                                 </ul>
                             </li>
->>>>>>> 0c99a4f21273c3150905c2a24b41d6996a1b6e2f
 
                             <li class="has-droupdown"><a href="#">{{__("site.Academic Degree")}}</a>
                                 <ul class="mega-menu">
@@ -171,21 +131,21 @@
                             </a>
                         </li> --}}
                         @if (lang()== 'ar')
-                        <li>
-                            <a href="{{route('lang',['en'])}}">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="22" height="22">
-                                    <path d="M2.04932 13.0001H7.52725C7.70624 16.2689 8.7574 19.3054 10.452 21.881C5.98761 21.1872 2.5001 17.5403 2.04932 13.0001ZM2.04932 11.0001C2.5001 6.4598 5.98761 2.81288 10.452 2.11914C8.7574 4.69468 7.70624 7.73123 7.52725 11.0001H2.04932ZM21.9506 11.0001H16.4726C16.2936 7.73123 15.2425 4.69468 13.5479 2.11914C18.0123 2.81288 21.4998 6.4598 21.9506 11.0001ZM21.9506 13.0001C21.4998 17.5403 18.0123 21.1872 13.5479 21.881C15.2425 19.3054 16.2936 16.2689 16.4726 13.0001H21.9506ZM9.53068 13.0001H14.4692C14.2976 15.7829 13.4146 18.3733 11.9999 20.5916C10.5852 18.3733 9.70229 15.7829 9.53068 13.0001ZM9.53068 11.0001C9.70229 8.21722 10.5852 5.62684 11.9999 3.40853C13.4146 5.62684 14.2976 8.21722 14.4692 11.0001H9.53068Z" fill="rgba(255,0,0,1)"></path>
-                                </svg>
-                            </a>
-                        </li>
+                            <li>
+                                <a href="{{route('lang',['en'])}}">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="22" height="22">
+                                        <path d="M2.04932 13.0001H7.52725C7.70624 16.2689 8.7574 19.3054 10.452 21.881C5.98761 21.1872 2.5001 17.5403 2.04932 13.0001ZM2.04932 11.0001C2.5001 6.4598 5.98761 2.81288 10.452 2.11914C8.7574 4.69468 7.70624 7.73123 7.52725 11.0001H2.04932ZM21.9506 11.0001H16.4726C16.2936 7.73123 15.2425 4.69468 13.5479 2.11914C18.0123 2.81288 21.4998 6.4598 21.9506 11.0001ZM21.9506 13.0001C21.4998 17.5403 18.0123 21.1872 13.5479 21.881C15.2425 19.3054 16.2936 16.2689 16.4726 13.0001H21.9506ZM9.53068 13.0001H14.4692C14.2976 15.7829 13.4146 18.3733 11.9999 20.5916C10.5852 18.3733 9.70229 15.7829 9.53068 13.0001ZM9.53068 11.0001C9.70229 8.21722 10.5852 5.62684 11.9999 3.40853C13.4146 5.62684 14.2976 8.21722 14.4692 11.0001H9.53068Z" fill="rgba(255,0,0,1)"></path>
+                                    </svg>
+                                </a>
+                            </li>
                         @else
-                        <li>
-                            <a href="{{route('lang',['ar'])}}">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="22" height="22">
-                                    <path d="M2.04932 13.0001H7.52725C7.70624 16.2689 8.7574 19.3054 10.452 21.881C5.98761 21.1872 2.5001 17.5403 2.04932 13.0001ZM2.04932 11.0001C2.5001 6.4598 5.98761 2.81288 10.452 2.11914C8.7574 4.69468 7.70624 7.73123 7.52725 11.0001H2.04932ZM21.9506 11.0001H16.4726C16.2936 7.73123 15.2425 4.69468 13.5479 2.11914C18.0123 2.81288 21.4998 6.4598 21.9506 11.0001ZM21.9506 13.0001C21.4998 17.5403 18.0123 21.1872 13.5479 21.881C15.2425 19.3054 16.2936 16.2689 16.4726 13.0001H21.9506ZM9.53068 13.0001H14.4692C14.2976 15.7829 13.4146 18.3733 11.9999 20.5916C10.5852 18.3733 9.70229 15.7829 9.53068 13.0001ZM9.53068 11.0001C9.70229 8.21722 10.5852 5.62684 11.9999 3.40853C13.4146 5.62684 14.2976 8.21722 14.4692 11.0001H9.53068Z" fill="rgba(255,0,0,1)"></path>
-                                </svg>
-                            </a>
-                        </li>
+                            <li>
+                                <a href="{{route('lang',['ar'])}}">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="22" height="22">
+                                        <path d="M2.04932 13.0001H7.52725C7.70624 16.2689 8.7574 19.3054 10.452 21.881C5.98761 21.1872 2.5001 17.5403 2.04932 13.0001ZM2.04932 11.0001C2.5001 6.4598 5.98761 2.81288 10.452 2.11914C8.7574 4.69468 7.70624 7.73123 7.52725 11.0001H2.04932ZM21.9506 11.0001H16.4726C16.2936 7.73123 15.2425 4.69468 13.5479 2.11914C18.0123 2.81288 21.4998 6.4598 21.9506 11.0001ZM21.9506 13.0001C21.4998 17.5403 18.0123 21.1872 13.5479 21.881C15.2425 19.3054 16.2936 16.2689 16.4726 13.0001H21.9506ZM9.53068 13.0001H14.4692C14.2976 15.7829 13.4146 18.3733 11.9999 20.5916C10.5852 18.3733 9.70229 15.7829 9.53068 13.0001ZM9.53068 11.0001C9.70229 8.21722 10.5852 5.62684 11.9999 3.40853C13.4146 5.62684 14.2976 8.21722 14.4692 11.0001H9.53068Z" fill="rgba(255,0,0,1)"></path>
+                                    </svg>
+                                </a>
+                            </li>
                         @endif
                         <li class="mobile-menu-bar d-block d-xl-none">
                             <button class="hamberger-button">
@@ -243,7 +203,7 @@
                             <ul class="submenu mega-sub-menu-01">
                                 <li>
                                     <a href="#">
-                                      
+
                                         {{-- <img src="{{asset('site/assets/images/others/mega-menu-image.webp')}}" alt="advertising Image"> --}}
                                         <img src="  {{ asset('site/logo.png') }}" alt="advertising Image">
                                     </a>
@@ -265,24 +225,24 @@
                                 <li><a href="team-three.html">Instructor 3</a></li>
                                 <li><a href="team-details.html">Instructor Profile</a></li>
                                 <li><a href="faq.html">Faq's</a></li>
-                               
+
                             </ul>
                         </li>
-                       
-                     
+
+
                     </ul>
                 </li>
 
 
-              
+
                 <li class="has-droupdown"><a href="#">{{__("site.Admission and Registration")}}</a>
                     <ul class="submenu">
-                        
+
                         <li><a href="course-one.html">{{__("site.About Admission")}}</a></li>
                         <li><a href="course-two.html">{{__("site.Grading System")}}</a></li>
                         <li><a href="course-three.html">{{__("site.New Students")}}</a></li>
                         <li><a href="course-four.html">{{__("site.Frequently Asked Questions")}}</a></li>
-                        <li><a href="course-five.html">{{__("site.Students’ Guidelines")}}</a></li>                       
+                        <li><a href="course-five.html">{{__("site.Students’ Guidelines")}}</a></li>
                     </ul>
                 </li>
 
@@ -290,7 +250,7 @@
                     <ul class="submenu">
                         <li><a href="blog-standard.html">{{__("site.Bachelor")}}</a></li>
                         <li><a href="blog-masonry.html">{{__("site.Diploma")}}</a></li>
-                        
+
                     </ul>
                 </li>
                 <li class=""><a href="#">{{__("site.Research")}}</a>
